2a886447867f263403cf9f3e7e23ff57e9315830
David Sousa <david.anderson.lino@gmail.com>
2020-03-04 08:01:46 +0100

    feat:Add interfaces for NotifierService

--- types/services.d.ts
+++ types/services.d.ts
@@ -1,3 +1,7 @@
+import { User } from 'server/models/User';
+import { Event } from 'server/models/Event';
+import { Notification } from 'server/models/Notification';
+
 export interface IMailerService {
   emailList: Array<string>;
   subject: string;
@@ -9,4 +13,31 @@ export interface IMailerService {
   emailPassword: string;
   emailService: string;
   sendEmail: () => void;
+}
+
+/**
+ * This interface should be implemented by all classes that react to an RSVP event
+ */
+export interface IRSVPListener {
+  userChangedRSVP: (user: User, event: Event) => void;
+}
+
+/**
+ * This interface should be implemented by all classes that react to an User updating its settings
+ */
+export interface IUserSettingUpdateListener {
+  settingsUpdated: (
+    userBefore: User,
+    userAfter: User,
+    updatedFields: Array<string>,
+  ) => void;
+}
+
+/**
+ * This interface represents a service capable of handling notifications
+ */
+export interface INotifierService {
+  sendNotifications: () => void;
+  removeNotifications: (notification: Notification) => void;
+  addNotifications: (notification: Notification) => void;
 }
